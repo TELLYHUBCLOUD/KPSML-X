6a958cd666153319631cd658a7b8cc90a26bb911
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-11-25 09:17:39 +0530

    Update update.py

--- update.py
+++ update.py
@@ -56,8 +56,8 @@ if len(DATABASE_URL) == 0:
 
 if DATABASE_URL is not None:
     conn = MongoClient(DATABASE_URL)
-    db = conn.kpsmlx
-    old_config = db.settings.deployConfig.find_one({'_id': bot_id})
+    db = conn.[f'tellykp{bot_id}']
+    old_config = db.settings.deployConfig.find_one({'_id': })
     config_dict = db.settings.config.find_one({'_id': bot_id})
     if old_config is not None:
         del old_config['_id']
@@ -75,7 +75,7 @@ if UPGRADE_PACKAGES.lower() == 'true':
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = "https://github.com/Tamilupdates/KPSML-X"
+    UPSTREAM_REPO = "https://github.com/TELLYHUBCLOUD/KPSML-X"
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
